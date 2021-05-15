6d5f61cf65e5138bdf68d44b9cae84f4297d1ebc
Michael86868 <michael.kuhnel8@gmail.com>
2021-05-15 21:54:15 +0200

    Added User CRUD

---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('birthdate', BirthdayType::class)
+            ->add('gender', ChoiceType::class)
+            ->add('industry', TextType::class)
+            ->add('password', TextType::class)
+            ->add('city', TextType::class)
+            ->add('bio', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
