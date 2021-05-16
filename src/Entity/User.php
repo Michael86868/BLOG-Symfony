60309b5df757bd8dda553c190d6c0050238b014c
Michael86868 <michael.kuhnel8@gmail.com>
2021-05-16 12:20:13 +0200

    Modifed Unique Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -58,7 +58,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="boolean")
      */
-    private bool $isVerified;
+    private bool $isVerified = false;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -191,7 +191,7 @@ class User implements UserInterface
 
     public function setIsVerified(bool $isVerified): self
     {
-        $this->isVerified = true;
+        $this->isVerified = $isVerified;
 
         return $this;
     }
