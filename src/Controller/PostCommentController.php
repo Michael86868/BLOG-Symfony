8378c0f35e90458bb7bc85c89f27d7828ee47914
Michael86868 <michael.kuhnel8@gmail.com>
2021-05-30 16:30:21 +0200

    Added design and delete action for post comments

---
+++ src/Controller/PostCommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PostComments;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostCommentController extends AbstractController
+{
+    /**
+     * @Route("/post/comment/{id}", name="delete_comment", methods={"POST"})
+     */
+    public function delete(Request $request, PostComments $postComments): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$postComments->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($postComments);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('post_show', [
+            'id' => $postComments->getPost()->getId(),
+        ]);
+    }
+}
